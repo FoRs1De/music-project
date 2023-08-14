776dd8bf2ef68dc589e5859c886f162738ac61ae
blkjacobinsjr <124602974+blkjacobinsjr@users.noreply.github.com>
2023-08-14 13:50:30 +0200

    relaxation fix

--- src/components/Ralaxation.jsx
+++ src/components/Ralaxation.jsx
@@ -2,14 +2,13 @@ import React, { useRef, useState, useEffect } from 'react';
 import SpotifyPlayer from 'react-spotify-web-playback';
 
 const Relaxation = () => {
-  const vimeoRef = useRef(null); // Reference to the Vimeo player
-  const [play, setPlay] = useState(false); // State to manage the play/pause of the Vimeo player
+  const vimeoRef = useRef(null);
+  const [play, setPlay] = useState(false);
 
-  const token = 'BQCirpQj8r5j4hu6KielQ3IG-Sbfb5GD4CfruDKfnqfs-n_SDYLqFvIVnptdL_B1B8cQEWtel9jnU3M-WC2iJSM2s-i9FifDIMduH3O8zDAl3aqw7ZgDtntkUzKstsYh-VdZWQ68KJ6lBAQyWSrrYyJlFpa0YYNAMsxe_dHx00jjiGp38FJmIIVez3OX6MUtu4sNpJa8vZo'; // Spotify access token
-  const trackUri = 'spotify:track:04boE4u1AupbrGlI62WvoO'; // Add Spotify track URL here
+  const token = 'BQCPaXDipGLw_QMRrhhY7biyyqaLDW01WHYpzclnzWvsuZkqfMgtL87iKtw5Iynw1p3kujXEMl569of7zT7aaV4LAZ1LkNSFQeHOgsWusbMCsHhdfnnzy5tKTgNhXkklGdDKjiQoZxkGT_Ro9giqyxjLL4w0LcO8SzTYJVVUmMHtOMw7BN8aAcFu8SBQiMUIdeDASEn3eIQ';
+  const trackUri = 'spotify:track:04boE4u1AupbrGlI62WvoO';
 
   useEffect(() => {
-    // Control the playback of the Vimeo player based on the state of the Spotify player
     if (vimeoRef.current && play) {
       vimeoRef.current.contentWindow.postMessage({ method: 'play' }, '*');
     } else if (vimeoRef.current && !play) {
@@ -21,7 +20,6 @@ const Relaxation = () => {
     <>
       {/* Spotify Player */}
       <div style={{ position: 'absolute', top: 10, right: 10, zIndex: 1000 }}>
-        {/* Render the Spotify player */}
         <SpotifyPlayer
           token={token}
           uris={[trackUri]}
@@ -38,7 +36,6 @@ const Relaxation = () => {
           hideCoverArt
           inlineVolume
           callback={state => {
-            // Update the state of the Vimeo player based on the Spotify player's state
             if (state.isPlaying !== play) {
               setPlay(state.isPlaying);
             }
@@ -49,7 +46,7 @@ const Relaxation = () => {
       {/* Vimeo Player */}
       <iframe
         ref={vimeoRef}
-        src="https://player.vimeo.com/video/93050095?loop=1&background=1" //Add your vimeo link here
+        src="https://player.vimeo.com/video/93050095?loop=1&background=1"
         style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
         frameBorder="0"
         allow="autoplay; fullscreen"
