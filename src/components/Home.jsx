8a5263b6672a55c5c4a1163d454f73c0c7c11a6b
blkjacobinsjr <124602974+blkjacobinsjr@users.noreply.github.com>
2023-08-14 16:03:46 +0200

    homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,25 @@
+import { useRef, useEffect } from 'react';
+
 const Home = () => {
-  return <></>;
+  const vimeoRef = useRef(null);
+
+  useEffect(() => {
+    if (vimeoRef.current) {
+      vimeoRef.current.contentWindow.postMessage({ method: 'play' }, '*');
+    }
+
+    return () => {
+      if (vimeoRef.current) {
+        vimeoRef.current.contentWindow.postMessage({ method: 'pause' }, '*');
+      }
+    };
+  }, []);
+
+  return (
+    <>
+      <h1 style={{ textAlign: 'center', fontWeight: 'bold' }}>Welcome to Channel 3</h1>
+    </>
+  );
 };
 
 export default Home;
