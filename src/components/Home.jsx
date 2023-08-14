efab67ad54d490b75ecb84c7cdff22d12492b4aa
blkjacobinsjr <124602974+blkjacobinsjr@users.noreply.github.com>
2023-08-14 16:03:50 +0200

    homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -18,6 +18,12 @@ const Home = () => {
   return (
     <>
       <h1 style={{ textAlign: 'center', fontWeight: 'bold' }}>Welcome to Channel 3</h1>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <img src="https://media.discordapp.net/attachments/819961405037084683/1140645309127589918/Mein_Bild.jpg?width=684&height=456" alt="Image 1" style={{ width: '300px', margin: '10px' }} />
+        <img src="https://media.discordapp.net/attachments/819961405037084683/1140645374948806727/Screenshot_2023-08-14_155709.png?width=421&height=456" alt="Image 2" style={{ width: '300px', margin: '10px' }} />
+        <img src="https://media.discordapp.net/attachments/819961405037084683/1140646071870181446/Screenshot_2023-08-14_at_15.59.32.png" alt="Image 3" style={{ width: '300px', margin: '10px' }} />
+      </div>
+
     </>
   );
 };
